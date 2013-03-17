8e140ca2a572a9aabd6c576b1b994d0ecb9b0dbd
dawn110110 <dawn110110@gmail.com>
2013-03-18 00:48:10 +0800

    begin writing wrapper scirpt

--- main.py
+++ main.py
@@ -3,16 +3,27 @@
 
 from parser import CodeSoup
 import sys
+import shutil
 
+def main():
+    try:
+        src = sys.argv[1]
+    except:
+        print '1st parameter is source directory,\
+               you must speifily an avaliable project  path'
+        return
+
+    dst = src+'_clean'
+    print 'src = %r' % src
+    print 'dst = %r' % dst
 if __name__ == "__main__":
-    argv = sys.argv
-    txt = open("./testfiles/abstract.c").read()
+    main()
 
-    s = CodeSoup(txt)
-    print 'comment line = %r' % s.comments_line_count
-    print 'blank line = %r' % s.blank_line_count
-    clean_txt = s.clean
-
-    f = open("./clean.txt", 'w')
-    f.write(clean_txt)
-    f.close()
+#    s = CodeSoup(txt)
+#    print 'comment line = %r' % s.comments_line_count
+#    print 'blank line = %r' % s.blank_line_count
+#    clean_txt = s.clean
+#
+#    f = open("./testfiles/abstract_clean.c", 'w')
+#    f.write(clean_txt)
+#    f.close()
